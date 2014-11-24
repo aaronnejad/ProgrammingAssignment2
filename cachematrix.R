7c5cdf2575c1d6254ee812f0c73bef25b471c6d7
Roni <roninemat@gmail.com>
2014-11-23 19:23:07 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This is Assignement 2
+## functions called  makeCacheMatrix   and CasheSolve
 
-## Write a short comment describing this function
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+    
+  }
+  get<-function()x
+  setmatrix<-function(inverse)   m<<-inverse
+  getmatrix<-function()m
+  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x...) {
+  ## Return a matrix that is the inverse of 'x'
+  
+  m<-x$getinverse()
+  if (!is.null(m){
+    message("getting   cashed data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix)
+  x$setm(inv)
+  m
 }
